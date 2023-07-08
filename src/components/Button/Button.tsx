39a5b6dd3c832d8ee97f79b3589f9a58fe504972
treheinguy <treheinguy@gmail.com>
2023-07-07 20:15:29 -0500

    init commit with test, build, and publish

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+export const Button: React.FC = () => {    
+    return (
+        <div>
+            This is a Button
+        </div>
+    )
+}
---
+++ src/stories/AltButton.stories.js
@@ -0,0 +1,12 @@
+import { Button } from '../components'
+
+export default {
+    title: 'components/AltButton',
+    component: Button
+}
+
+export const AltButton = {
+    render: () => <Button />,
+    // args: { /* ... */ },
+    // parameters: {  /* ... */ },
+  };
